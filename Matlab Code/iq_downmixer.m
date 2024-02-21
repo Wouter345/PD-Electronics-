2a2bdb3a6850c925cdef800f92f45c094485ab3d
unknown <wouter.globevnik1@gmail.com>
2024-02-21 17:30:31 +0100

    CIC filter cleaned code

--- Matlab Code/iq_downmixer.m
+++ Matlab Code/iq_downmixer.m
@@ -1,9 +1,7 @@
 function complex_envelope = iq_downmixer(signal, osr, br, fc, fs)
 
-% TODO: You may want to implement a better downsampling filter.
-% TIP: Look up 'CIC' filters as inspiration for an efficient hardware 
-% implementation. Also think about how you will generate the LO signal in
-% hardware, looking up 'CORDIC' will set you in a correct direction.
+% TODO: Mixer needs to be implemented with CORDIC algorithm
+% DONE: CIC Downsampling filter, 1 stage
 
 % IQ downmixer
 t = ((1 : numel(signal))' - 1) / fs;
@@ -11,39 +9,8 @@ upsampled_envelope = 2 * exp(-1j * 2 * pi * fc * t) .* signal;
 I_signal = real(upsampled_envelope);
 Q_signal = imag(upsampled_envelope);
 
-
-% CIC WITHOUT DOWNSAMPLING
-D = 32; % decimation factor
-N = 64; % delay buffer depth
-delayBufferI = zeros(1,N); % init
-delayBufferQ = zeros(1,N); % init
-intOutI = 0;
-intOutQ = 0;
-I_downsampled = [];
-Q_downsampled = [];
-for i = 1:length(I_signal)
-% comb section
-combOutI = I_signal(i) - delayBufferI(end);
-combOutQ = Q_signal(i) - delayBufferQ(end);
-delayBufferI(2:end) = delayBufferI(1:end-1);
-delayBufferQ(2:end) = delayBufferQ(1:end-1);
-delayBufferI(1) = I_signal(i);
-delayBufferQ(1) = Q_signal(i);
-
-% integrator
-intOutI = intOutI + combOutI;
-intOutQ = intOutQ + combOutQ;
-I_downsampled = [ I_downsampled intOutI];  
-Q_downsampled = [ Q_downsampled intOutQ]; 
-end
-
-I_downsampled = I_downsampled(1:D:end);
-Q_downsampled = Q_downsampled(1:D:end);
-
-complex_envelope1 = I_downsampled + 1i*Q_downsampled;
-
 %CIC DOWNSAMPLING FILTER 
-D = 32; %Decimation factor
+D = fs / (br*osr); %Decimation factor, NEEDS TO BE AN INTEGER
 N = 64; % delay buffer depth
 delayBufferI = zeros(1,N/D);
 delayBufferQ = zeros(1,N/D);
@@ -79,21 +46,6 @@ complex_envelope = I_downsampled + 1i*Q_downsampled;
 
 
 
-
-% % COMPARE CIC IMPLEMENTATION TO SIMPLE EXAMPLE
-% 
-% % apply a simple downsampling filter
-% filt = ones(2 * round(fs / (br * osr)) + 1);
-% upsampled_envelope = conv(upsampled_envelope, filt / sum(filt), 'same');
-% 
-% % calculate number of output samples
-% n1 = numel(upsampled_envelope);
-% n2 = round((n1 - 1) * (br * osr) / fs) + 1;
-% 
-% % resample the complex envelope to the new sample rate
-% t1 = ((1 : n1)' - 1) / fs;
-% t2 = ((1 : n2)' - 1) / (br * osr);
-% complex_envelope = interp1(t1, upsampled_envelope, t2);
 
 
 
