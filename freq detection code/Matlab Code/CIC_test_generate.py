22df553d8a391bc44f4df7d4be38dd4b210017f2
unknown <wouter.globevnik1@gmail.com>
2024-03-13 17:42:04 +0100

    testbench

--- freq detection code/Matlab Code/CIC_test_generate.py
+++ freq detection code/Matlab Code/CIC_test_generate.py
@@ -6,8 +6,12 @@ input = np.array(data)  # Convert loaded data to NumPy array
 data1 = np.loadtxt('output.txt')
 output = np.array(data1)
 
+for i in range(50):
+    print(int(output[i]))
+
 with open("CIC_test.txt", 'w') as f:
     for i in range(len(input)):
+        f.write("//sample "+str(i)+"\n")
         f.write("sample_indicator <= 1;\n")
         f.write("in <= " + str(int(input[i]))+";\n")
         f.write("#(clk_period);\n")
